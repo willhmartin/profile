1d1c359921553debad29661ce044e9506c0cdea5
Will Martin <whmartinwork@outlook.com>
2020-12-23 14:29:43 +0800

    first launch

---
+++ trianglifyContainer.js
@@ -0,0 +1,13 @@
+
+
+
+function addTriangleTo(target) {
+  var dimensions = target.getClientRects()[0];
+  var pattern = Trianglify({
+    width: dimensions.width,
+    height: dimensions.height
+  });
+  target.style['background-image'] = 'url(' + pattern.png() + ')';
+}
+
+addTriangleTo(document.getElementById('container'));
